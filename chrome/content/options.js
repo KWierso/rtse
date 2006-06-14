92034baea5ac58ab1b352aa669dd45cb9d10518f
comrade693 <comrade693>
2006-06-14 22:00:12 +0000

    -Major overhaul with the options dialog -Finally got rid of the old config system (now fully using preferences!) -Added the default prefs to rtse-prefs.js

--- chrome/content/options.js
+++ chrome/content/options.js
@@ -16,7 +16,7 @@
  * The Initial Developer of the Original Code is
  * Shawn Wilsher
  *
- * Portions created by the Initial Developer are Copyright (C) 2005
+ * Portions created by the Initial Developer are Copyright (C) 2005-2006
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
@@ -26,157 +26,13 @@
 var gRTSE=Components.classes['@shawnwilsher.com/rtse;1']
                     .createInstance(Components.interfaces.nsIRTSE);
 function apply() {
-	var i;
 
-	/* Function used to store and changes to prefs */
-	this.config=new RTSE_Config();
-	this.config.load();
-	
-	var names=new Array();
-	var values=new Array();
-	
-	/* Username */
-	gRTSE.prefsSetString('extensions.rtse.username',document.getElementById('username').value);
-	
-	/* Password */
-	names.push('pwd');
-	values.push(document.getElementById('pwd').value);
-
-	/* Auto-Login */
-	if( document.getElementById('signin').checked )
-		gRTSE.prefsSetBool('extensions.rtse.autologin',true);
-	else
-		gRTSE.prefsSetBool('extensions.rtse.autologin',false);
-
-	/* Sponsor */
-	if( document.getElementById('sponsor').checked )
-		gRTSE.prefsSetBool('extensions.rtse.sponsor',true);
-	else
-		gRTSE.prefsSetBool('extensions.rtse.sponsor',false);
-
-	/* Themer */
-	if( document.getElementById('theme').checked ) {
-		gRTSE.prefsSetBool('extensions.rtse.themer',true);
-		gRTSE.prefsSetString('extensions.rtse.themeType',document.getElementById('themeType').selectedItem.value);
-	} else {
-		gRTSE.prefsSetBool('extensions.rtse.themer',false);
-	}
-
-	/* Link Fixer */
-	if( document.getElementById('linkFix').checked )
-		values.push('true');
-	else
-		values.push('false');
-	names.push('linkFix');
-
-	/* Topic Filter */
-	var ref=document.getElementById('forumList').childNodes;
-	for( i=(ref.length-1); i>=0; i-- ) {
-		if( ref[i].firstChild.tagName=='checkbox' ) {
-			names.push('show_ftopic_'+ref[i].firstChild.getAttribute('value'));
-			if( ref[i].firstChild.checked )
-				values.push('true');
-			else
-				values.push('false');
-		}
-	}
-
-	/* Editor */
-	if( document.getElementById('editor').checked )
-		gRTSE.prefsSetBool('extensions.rtse.editor',true);
-	else
-		gRTSE.prefsSetBool('extensions.rtse.editor',false);
-
-	/* Smilies */
-	if( document.getElementById('smilies').checked )
-		values.push('true');
-	else
-		values.push('false');
-	names.push('smilies');
-
-	/* Same-page Reply */
-	if( document.getElementById('same_page_reply').checked )
-		values.push('true');
-	else
-		values.push('false');
-	names.push('same_page_reply');
-
-	/* apply changes, change pref, and exit */
-	this.config.setMult(names,values);
-	gRTSE.prefsSetBool('reload',true,'extensions.rtse.config.');
 	return true;
 }
 
 function load() {
-	/* Function ran at load to set values */
-	this.config=new RTSE_Config();
-	this.config.load();
-
 	// Load Smilies Preview
 	displaySmilies();
-
-	var i;
-	
-	/* Username */
-	document.getElementById('username').value=gRTSE.prefsGetString('extensions.rtse.username');
-	
-	/* Password */
-	document.getElementById('pwd').value=this.config.get('pwd','');
-	
-	/* Auto-Login */
-	if( gRTSE.prefsGetBool('extensions.rtse.autologin')==false )
-		document.getElementById('signin').checked=false;
-	else
-		document.getElementById('signin').checked=true;
-	
-	/* Sponsor */
-	if( gRTSE.prefsGetBool('extensions.rtse.sponsor')==false )
-		document.getElementById('sponsor').checked=false;
-	else
-		document.getElementById('sponsor').checked=true;
-	
-	/* Themer */
-	if( gRTSE.prefsGetBool('extensions.rtse.themer') ) {
-		document.getElementById('themeType').value=gRTSE.prefsGetString('extensions.rtse.themeType');
-		document.getElementById('theme').checked=true;
-	} else {
-		document.getElementById('themeType').disabled=true;
-		document.getElementById('theme').checked=false;
-	}
-	document.getElementById('theme').addEventListener('CheckboxStateChange',theme,false);
-	
-	/* Link Fixer */
-	if( this.config.get('linkFix','true')=='true' )
-		document.getElementById('linkFix').checked=true;
-	else
-		document.getElementById('linkFix').checked=false;
-	
-	/* Topic Filter */
-	var ref=document.getElementById('forumList').childNodes;
-	for( i=(ref.length-1); i>=0; i-- ) {
-		if( ref[i].firstChild.tagName=='checkbox' && this.config.get('show_ftopic_'+ref[i].firstChild.getAttribute('value'),'true')=='true' )
-			ref[i].firstChild.checked=true;
-		else
-			ref[i].firstChild.checked=false;
-	}
-
-	/* Editor */
-	if( gRTSE.prefsGetBool('extensions.rtse.editor') )
-		document.getElementById('editor').checked=true;
-	else
-		document.getElementById('editor').checked=false;
-
-	/* Smilies */
-	if( this.config.get('smilies','true')=='true' )
-		document.getElementById('smilies').checked=true;
-	else
-		document.getElementById('smilies').checked=false;
-	
-	/* Same page reply */
-	if( this.config.get('same_page_reply','false')=='true' )
-		document.getElementById('same_page_reply').checked=true;
-	else
-		document.getElementById('same_page_reply').checked=false;
 }
 
 function theme() {
