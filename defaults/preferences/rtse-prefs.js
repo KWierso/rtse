2a4e9a2c5a9b13dead852d41e227daa0131e3859
KWierso <KWierso>
2009-04-14 23:14:21 +0000

    Bug 158 -  Add new secret forum to RTSE. Toggle default preference for tourney forum. -KWierso

--- defaults/preferences/rtse-prefs.js
+++ defaults/preferences/rtse-prefs.js
@@ -42,7 +42,7 @@ pref("extensions.rtse.forum.1",true);
 pref("extensions.rtse.forum.37",true);
 pref("extensions.rtse.forum.8",true);
 pref("extensions.rtse.forum.34",true);
-pref("extensions.rtse.forum.40",false);
+pref("extensions.rtse.forum.40",true);
 
 pref("extensions.rtse.forum.9",true);
 pref("extensions.rtse.forum.39",true); 
