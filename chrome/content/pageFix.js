05c80675c30f1da0d8474949a5497e0ca7f25e9e
kwierso <kwierso>
2009-05-05 05:35:14 +0000

    Bug 179 -  Page jump enhancement. Don't run on post edit pages. -KWierso

--- chrome/content/pageFix.js
+++ chrome/content/pageFix.js
@@ -59,7 +59,7 @@ function RTSE_pageJump(aDoc)
         try {
             if(elements[i].parentNode.parentNode.getElementsByTagName("a")[0].href.match(/page=1/) &&
                     !elements[i].parentNode.parentNode.innerHTML.match("new") && 
-                    !elements[i].parentNode.parentNode.innerHTML.match("Go Back") ) // Don't let watchlist items get added!
+                    !elements[i].parentNode.parentNode.innerHTML.match("Back") ) // Don't let watchlist items get added!
                 elementArray.push(elements[i].parentNode.parentNode);
         } catch(e) { }
     }
