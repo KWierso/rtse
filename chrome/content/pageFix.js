9e7532fa804e8960ad8dd3ed8ad6267ccf4c06b3
KWierso <KWierso>
2009-09-19 22:03:44 +0000

    Bug 257  -  Disable comment redirect on conversation pages

--- chrome/content/pageFix.js
+++ chrome/content/pageFix.js
@@ -570,10 +570,11 @@ function RTSE_samePageReply(aEvent)
                    .getElementsByTagName('a')[0].href.split("?uid=")[1];
     */
       }
-      if(uID != null && uID != "undefined") // Only modify the form if the uID can be found
-        RTSE_modifyForm(doc, "/members/comments/commentPost.php?uid=" + uID, doc.URL,
+      if(uID != null && uID != "undefined") { // Only modify the form if the uID can be found
+         if(doc.URL.match("/conversation.php") != "/conversation.php") // Don't redirect form if on a conversation page
+            RTSE_modifyForm(doc, "/members/comments/commentPost.php?uid=" + uID, doc.URL,
                   name + "'s Comments", "/members/comments/commentPost.php?uid=" + uID);
-      else {
+      } else {
         alert("This user's ID number cannot be found for some reason. " +
                          "You should probably reconsider hitting 'submit' " +
                          "before you make a fool of yourself.");
